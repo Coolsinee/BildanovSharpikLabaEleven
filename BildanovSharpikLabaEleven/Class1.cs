9dcaf145583ee63ed98a7c96b37ba58d00858868
RellTheSharpSlayer <emilbildanov@gmail.com>
2023-05-07 10:21:13 +0300

    Добавьте файлы проекта.

---
+++ BildanovSharpikLabaEleven/Class1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BildanovSharpikLabaEleven
+{
+    internal class Class1
+    {
+        public class Person : IComparable<Person>
+        {
+            private string lastName;
+            private string firstName;
+            private DateTime dateOfBirth;
+            private char gender;
+
+            public Person(string lastName, string firstName, DateTime dateOfBirth, char gender)
+            {
+                this.lastName = lastName;
+                this.firstName = firstName;
+                this.dateOfBirth = dateOfBirth;
+                Gender = gender;
+            }
+
+            public string LastName
+            {
+                get { return lastName; }
+                set { lastName = value; }
+            }
+
+            public string FirstName
+            {
+                get { return firstName; }
+                set { firstName = value; }
+            }
+
+            public DateTime DateOfBirth
+            {
+                get { return dateOfBirth; }
+                set { dateOfBirth = value; }
+            }
+
+            public char Gender
+            {
+                get { return gender; }
+                set
+                {
+                    if (value == 'M' || value == 'F')
+                        gender = value;
+                    else
+                        throw new ArgumentException("Invalid gender value.");
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"{LastName}, {FirstName} ({Gender}), born on {DateOfBirth.ToShortDateString()}";
+            }
+            public int CompareTo(Person other)
+            {
+                return LastName.CompareTo(other.LastName);
+            }
+            //public static Person GetMax(Person[] people)
+            //{
+            //    if (people == null || people.Length == 0)
+            //    {
+            //        throw new ArgumentException("Array is empty or null");
+            //    }
+
+            //    Person max = people[0];
+            //    for (int i = 1; i < people.Length; i++)
+            //    {
+            //        if (people[i].CompareTo(max) > 0)
+            //        {
+            //            max = people[i];
+            //        }
+            //    }
+
+            //    return max;
+            //}
+        }
+
+        public class AgeComparer : IComparer<Person>
+        {
+            public int Compare(Person x, Person y)
+            {
+                return x.DateOfBirth.CompareTo(y.DateOfBirth);
+            }
+        }
+
+        public class GenderComparer : IComparer<Person>
+        {
+            public int Compare(Person x, Person y)
+            {
+                return x.Gender.CompareTo(y.Gender);
+            }
+        }
+        public static class ArrayUtils
+        {
+            public static void Swap<T>(T[] array, int i, int j)
+            {
+                T temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+            public static T[] DeleteAtIndex<T>(T[] array, int index)
+            {
+                T[] result = new T[array.Length - 1];
+                Array.Copy(array, 0, result, 0, index);
+                Array.Copy(array, index + 1, result, index, array.Length - index - 1);
+                return result;
+            }
+        }
+        public class MyGenericClass<T>
+        {
+            private T value;
+
+            public MyGenericClass(T value)
+            {
+                this.value = value;
+            }
+
+            public T GetValue()
+            {
+                return value;
+            }
+        }
+        public class MyPair<T1, T2>
+        {
+            private T1 first;
+            private T2 second;
+
+            public MyPair(T1 first, T2 second)
+            {
+                this.first = first;
+                this.second = second;
+            }
+            public T1 First
+            {
+                get { return first; }
+                set { first = value; }
+            }
+            public T2 Second
+            {
+                get { return second; }
+                set { second = value; }
+            }
+            public object this[int index]
+            {
+                get
+                {
+                    if (index == 0)
+                    {
+                        return first;
+                    }
+                    else if (index == 1)
+                    {
+                        return second;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                set
+                {
+                    if (index == 0)
+                    {
+                        first = (T1)value;
+                    }
+                    else if (index == 1)
+                    {
+                        second = (T2)value;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            public override string ToString()
+            {
+                return $"({First}, {Second})";
+            }
+        }
+        public class MyTriple<T1, T2, T3> : MyPair<T1, T2>
+        {
+            private T3 third;
+
+            public MyTriple(T1 first, T2 second, T3 third) : base(first, second)
+            {
+                this.third = third;
+            }
+            public T3 Third
+            {
+                get { return third; }
+                set { third = value; }
+            }
+            public new object this[int index]
+            {
+                get
+                {
+                    if (index == 0)
+                    {
+                        return First;
+                    }
+                    else if (index == 1)
+                    {
+                        return Second;
+                    }
+                    else if (index == 2)
+                    {
+                        return Third;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                set
+                {
+                    if (index == 0)
+                    {
+                        First = (T1)value;
+                    }
+                    else if (index == 1)
+                    {
+                        Second = (T2)value;
+                    }
+                    else if (index == 2)
+                    {
+                        Third = (T3)value;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            public override string ToString()
+            {
+                return $"({First}, {Second}, {Third})";
+            }
+        }
+        class DoublyLinkedList<T>
+        {
+            class Node
+            {
+                public T Value { get; set; }
+                public Node Next { get; set; }
+                public Node Previous { get; set; }
+
+                public Node(T value, Node next = null, Node previous = null)
+                {
+                    Value = value;
+                    Next = next;
+                    Previous = previous;
+                }
+            }
+            private Node head;
+            private Node tail;
+            private int count;
+
+            public int Count { get { return count; } }
+
+            public T First
+            {
+                get
+                {
+                    if (head == null) throw new InvalidOperationException("List is empty.");
+                    return head.Value;
+                }
+            }
+            public T Last
+            {
+                get
+                {
+                    if (tail == null) throw new InvalidOperationException("List is empty.");
+                    return tail.Value;
+                }
+            }
+            public void AddFirst(T value)
+            {
+                Node newNode = new Node(value, head);
+                if (head != null)
+                {
+                    head.Previous = newNode;
+                }
+                else
+                {
+                    tail = newNode;
+                }
+                head = newNode;
+                count++;
+            }
+            public void AddLast(T value)
+            {
+                Node newNode = new Node(value, null, tail);
+                if (tail != null)
+                {
+                    tail.Next = newNode;
+                }
+                else
+                {
+                    head = newNode;
+                }
+                tail = newNode;
+                count++;
+            }
+            public override string ToString()
+            {
+                Node current = head;
+                string result = "";
+                while (current != null)
+                {
+                    result += current.Value.ToString() + " ";
+                    current = current.Next;
+                }
+                return result.TrimEnd();
+            }
+        }
+    }
+}
---
+++ BildanovSharpikLabaEleven/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BildanovSharpikLabaEleven.Class1;
+
+namespace BildanovSharpikLabaEleven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Первый тест
+            //int[] intArray = { 1, 2, 3, 4, 5 };
+            //ArrayUtils.Swap(intArray, 1, 3);
+            //Console.WriteLine(string.Join(", ", intArray));
+
+            //double[] doubleArray = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            //ArrayUtils.Swap(doubleArray, 2, 4);
+            //Console.WriteLine(string.Join(", ", doubleArray));
+
+            //string[] stringArray = { "one", "two", "three", "four", "five" };
+            //ArrayUtils.Swap(stringArray, 0, 4);
+            //Console.WriteLine(string.Join(", ", stringArray));
+
+            //Person[] personArray = new Person[]
+            //{
+            //    new Person("Doe", "John", new DateTime(1990, 5, 7), 'M'),
+            //    new Person("Smith", "Jane", new DateTime(1995, 2, 14), 'F'),
+            //    new Person("Johnson", "Bobson", new DateTime(1985, 11, 23), 'M')
+            //};
+            //ArrayUtils.Swap(personArray, 0, 2);
+            //foreach (Person p in personArray)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //numbers = ArrayUtils.DeleteAtIndex(numbers, 2);
+            //Console.WriteLine(string.Join(", ", numbers)); 
+
+            //string[] names = { "John", "Jane", "Bobson", "Alice" };
+            //names = ArrayUtils.DeleteAtIndex(names, 1);
+            //Console.WriteLine(string.Join(", ", names)); 
+
+            //Person[] persons = new Person[]
+            //{
+            //    new Person("Doe", "John", new DateTime(1990, 5, 7), 'M'),
+            //    new Person("Smith", "Jane", new DateTime(1995, 2, 14), 'F'),
+            //    new Person("Johnson", "Bobson", new DateTime(1985, 11, 23), 'M')
+            //};
+            //persons = ArrayUtils.DeleteAtIndex(persons, 2);
+            //foreach (Person p in persons)
+            //{
+            //    Console.WriteLine(p);
+            //}
+            ////Второй тест
+            //Person[] persons = new Person[]
+            //{
+            //    new Person("Doe", "John", new DateTime(1990, 5, 7), 'M'),
+            //    new Person("Smith", "Jane", new DateTime(1995, 2, 14), 'F'),
+            //    new Person("Johnson", "Bobson", new DateTime(1985, 11, 23), 'M')
+            //};
+            //Array.Sort(persons);
+            //foreach (Person p in persons)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            //Person maxPerson = GetMax(persons);
+            //Console.WriteLine($"Max person: {maxPerson}");
+
+            //Array.Sort(persons, new AgeComparer());
+            //foreach (Person p in persons)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            //Array.Sort(persons, new GenderComparer());
+            //foreach (Person p in persons)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            //static Person GetMax(Person[] array)
+            //{
+            //    Person max = array[0];
+            //    for (int i = 1; i < array.Length; i++)
+            //    {
+            //        if (array[i].CompareTo(max) > 0)
+            //        { 
+            //            max = array[i];
+            //        }
+            //    }
+            //    return max;
+            //}
+            ////Третий тест
+            //MyPair<int, string> pair = new MyPair<int, string>(1, "one");
+            //Console.WriteLine(pair);
+            //Console.WriteLine(pair[0]);
+            //Console.WriteLine(pair[1]);
+            //pair[0] = 2;
+            //pair[1] = "two";
+            //Console.WriteLine(pair);
+
+            //MyTriple<double, string, bool> triple = new MyTriple<double, string, bool>(1.0, "one", true);
+            //Console.WriteLine(triple);
+            //Console.WriteLine(triple[0]);
+            //Console.WriteLine(triple[1]);
+            //Console.WriteLine(triple[2]);
+            //triple[0] = 2.0;
+            //triple[1] = "two";
+            //triple[2] = false;
+            //Console.WriteLine(triple);
+            ////Четвёртый тест
+            //DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+            //Console.WriteLine($"Count: {list.Count}");
+            //list.AddFirst(1);
+            //Console.WriteLine($"Count: {list.Count}, First: {list.First}, Last: {list.Last}");
+            //list.AddFirst(2);
+            //Console.WriteLine($"Count: {list.Count}, First: {list.First}, Last: {list.Last}");
+            //list.AddLast(3);
+            //Console.WriteLine($"Count: {list.Count}, First: {list.First}, Last: {list.Last}");
+            //Console.WriteLine(list);
+        }
+    }
+}
